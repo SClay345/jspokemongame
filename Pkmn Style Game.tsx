8ae5b8d060a0ba30a1d06c2040bc01d8ab92deb2
SClay345 <clayms345@gmail.com>
2022-04-11 15:59:20 -0700

    initial project commit

---
+++ Pkmn Style Game.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="Pkmn Style Game" tilewidth="12" tileheight="12" tilecount="1024" columns="32">
+ <image source="Chris Courses - Pokemon-20220408T211809Z-001/Chris Courses - Pokemon/Tiled/tileset.png" width="384" height="384"/>
+</tileset>
