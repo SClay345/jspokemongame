8b334c0c624d0600a3acac12e7a818cc87f40687
Clay Schneider <clayms345@gmail.com>
2022-04-11 22:27:34 -0700

    cleaned up directory, added code start, added map and player movement

---
+++ Game Assets/Maps/collision.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="collision" tilewidth="12" tileheight="12" tilecount="1" columns="1">
+ <image source="Chris Courses - Pokemon-20220408T211809Z-001/Chris Courses - Pokemon/Tiled/collision.png" width="12" height="12"/>
+</tileset>
